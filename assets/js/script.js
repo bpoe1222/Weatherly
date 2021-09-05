6fcefd0e7af175c3cab06e5443e12b41073c66bb
Bailey Poe <baileypoe1222@gmail.com>
2021-09-05 14:15:22 -0500

    boneworks of site

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var latitude = "33.44";
+var longitude = "-94.04";
+var exclude = "hourly,daily";
+var weaterAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=" + exclude + "&appid=61c240599847e191918ff8288b392a46";
+
